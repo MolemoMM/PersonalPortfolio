486904cc22dca185945b8cf63b365eb44e6431a6
MolemoMM <mamashelamolemo@gmail.com>
2024-11-15 15:36:31 +0200

    latest updates

--- scrips/jason.js
+++ scrips/jason.js
@@ -4,4 +4,30 @@ window.addEventListener("scroll",function() {
     header.classList.toggle("sticky", window.scrollY > 130);
 });
 
+let menu = document.querySelector('#menu-icon');
+let menulist = document.querySelector('.menulist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    menulist.classList.toggle('open');
+};
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    menulist.classList.remove('open');
+
+};
+
+var typed = new Typed(".input",{
+    strings:[
+        "Backend Developer.","Mobile Developer."
+    ],
+    typeSpeed:120,
+    backSpeed: 70,
+    loop: true
+
+});
+
+
+
+
 
