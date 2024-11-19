483b00525935de127005a259d364de23e540bfb4
MolemoMM <mamashelamolemo@gmail.com>
2024-11-19 16:24:20 +0200

    latest

--- scrips/jason.js
+++ scrips/jason.js
@@ -19,7 +19,7 @@ window.onscroll = () => {
 
 var typed = new Typed(".input",{
     strings:[
-        "Backend Developer.","Mobile Developer."
+        "Backend Developer.","Mobile App Developer."
     ],
     typeSpeed:120,
     backSpeed: 70,
